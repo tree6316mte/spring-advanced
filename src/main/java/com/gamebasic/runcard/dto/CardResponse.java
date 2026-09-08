2361f5f17902c245efce192b04bb1c4aabdf6b73
박진우 <tree6316mte@gmail.com>
2026-09-08 13:22:03 +0900

    [Fix] 요청 검증과 응답 DTO: 게임 생성

--- src/main/java/com/gamebasic/runcard/dto/CardResponse.java
+++ src/main/java/com/gamebasic/runcard/dto/CardResponse.java
@@ -5,7 +5,13 @@ import lombok.Getter;
 @Getter
 public class CardResponse {
     // TODO (Lv 5): API 명세의 카드 응답 JSON에 맞게 필드를 만들고 생성자에서 채우세요.
+    Long id;
+    String cardType;
+    int acquiredFloor;
 
     public CardResponse(Long id, String cardType, int acquiredFloor) {
+        this.id = id;
+        this.cardType = cardType;
+        this.acquiredFloor = acquiredFloor;
     }
 }
--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,18 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+
+    @NotBlank
     private String cardType;
-    private Integer acquiredFloor;
+
+    @Min(0)
+    @Max(10)
+    private Integer acquiredFloor = 0;
 }
